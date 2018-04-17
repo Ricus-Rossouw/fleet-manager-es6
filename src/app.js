cd8715fc86de6f44a84914c42a2ecd885b7e5997
Ricus Rossouw <ricochet@pool-100-1-0-18.nwrknj.fios.verizon.net>
2018-04-17 12:43:38 +0200

    created title bar class

--- src/app.js
+++ src/app.js
@@ -8,5 +8,13 @@ import { fleet } from './fleet-data.js';
 import { FleetDataService } from './services/fleet-data-service.js';
 
 // UI Classes
+import { TitleBar } from './ui/title-bar.js';
 import { Button } from './ui/button.js';
 import { Image } from './ui/image.js';
+
+let titleBar = new TitleBar('Application');
+titleBar.addLink('Home', '');
+titleBar.addLink('Cars', '');
+titleBar.addLink('Drones', '');
+titleBar.addLink('Map', '');
+titleBar.appendElement($('body'));
